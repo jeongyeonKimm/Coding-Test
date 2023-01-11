b4808f54ad89a9686321d06ee13be1b493c78722
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-01-11 21:49:09 +0900

    계수 정렬 예제

---
+++ ch6/6-4.java
@@ -0,0 +1,19 @@
+public class Main4 {
+
+    public static final int MAX_VALUE = 9;
+
+    public static void main(String[] args) {
+        int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2};
+        int[] count = new int[MAX_VALUE + 1];
+
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+
+        for (int i = 0; i <= MAX_VALUE; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

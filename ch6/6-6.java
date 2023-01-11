61dca3296dd9abd397f5593a4faf4ff93fda9294
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-01-11 22:24:41 +0900

    성적이 낮은 순서로 학생 출력하기

---
+++ ch6/6-6.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class Student implements Comparable<Student> {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        if (this.score < other.score) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int score = sc.nextInt();
+            students.add(new Student(name, score));
+        }
+
+        Collections.sort(students);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(students.get(i).getName() + " ");
+        }
+    }
+}

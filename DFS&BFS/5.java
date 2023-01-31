9501f29af773571bb58b30a4364147787d1afce9
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2023-01-31 16:29:17 +0900

    연산자 끼워 넣기

--- DFS&BFS/5.java
+++ DFS&BFS/5.java
@@ -1,1 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main5 {
+
+    public static int n;
+    public static ArrayList<Integer> arr = new ArrayList<>();
+    public static int add, sub, mul, div;   // 연산자의 개수
+
+    public static int minValue = (int) 1e9;
+    public static int maxValue = (int) -1e9;
+
+    public static void dfs(int i, int now) {
+        if (i == n) {
+            maxValue = Math.max(maxValue, now);
+            minValue = Math.min(minValue, now);
+        }
+        else {
+            if (add > 0) {
+                add -= 1;
+                dfs(i + 1, now + arr.get(i));
+                add += 1;
+            }
+            if (sub > 0) {
+                sub -= 1;
+                dfs(i + 1, now - arr.get(i));
+                sub += 1;
+            }
+            if (mul > 0) {
+                mul -= 1;
+                dfs(i + 1, now * arr.get(i));
+                mul += 1;
+            }
+            if (div > 0) {
+                div -= 1;
+                dfs(i + 1, now / arr.get(i));
+                div += 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            arr.add(x);
+        }
+
+        add = sc.nextInt();
+        sub = sc.nextInt();
+        mul = sc.nextInt();
+        div = sc.nextInt();
+
+        dfs(1, arr.get(0));
+
+        System.out.println(maxValue);
+        System.out.println(minValue);
+    }
+}
 
